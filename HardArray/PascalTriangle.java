5d99b172a47498a67d3fe2aace226ff5c5319d01
Arshdeep <harkiratkaur8c6@gmail.com>
2025-11-16 17:00:44 +0530

    Completed Type 1 category of Pascal triangle's optimal solution

--- HardArray/PascalTriangle.java
+++ HardArray/PascalTriangle.java
@@ -2,8 +2,29 @@ import java.util.*;
 
 public class PascalTriangle {
     
+
+    // Type - 1 : Given row and col, provide the value at that position in pascal triangle
+    public static int pascalValue(int row, int col){
+        if(col == 0 || col == row){
+            return 1;
+        }
+       
+        // optimal way to calculate nCr = n! / (r! * (n-r)!)
+        int deno = 1;
+        int num = row;
+        int result = 1;
+        for(int i = 0; i<col-1; i++){
+            deno = i+1;
+            num = num - 1;
+            result = (result * num) / deno;
+        }
+
+        return result;
+    }
+
+    // Type - 3 : Given number of rows, provide the pascal triangle till that number of rows
     // Only Solution Possible => Time Complexity: O(n^2) Space Complexity: O(1) ignoring output space
-    public static List<List<Integer>> generate(int numRows) {
+    /* public static List<List<Integer>> generate(int numRows) {
         List<List<Integer>> main = new Vector<>();
 
         if(numRows == 0){
@@ -38,15 +59,21 @@ public class PascalTriangle {
         }
 
         return main;
-    }
+    } */
     public static void main(String [] args){
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter number of rows you want of pascal triangle: ");
+        /* System.out.println("Enter number of rows you want of pascal triangle: ");
         int rows = input.nextInt();
 
         List<List<Integer>> pascalList = generate(rows);
 
-        System.out.println(pascalList);
+        System.out.println(pascalList); */
+
+        System.out.println("Enter row and col of where you want to find value: ");
+        int row = input.nextInt();
+        int col = input.nextInt();
+
+        System.out.println("The value present at that row and col is: " + pascalValue(row, col));
     }
 }
