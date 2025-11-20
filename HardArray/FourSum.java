be6b7ac1cfd3e6b18f9c75ccb5546ac14bcf7c43
Arshdeep <harkiratkaur8c6@gmail.com>
2025-11-20 12:20:51 +0530

    Completed Four Sum problem with Optimal Approach Solution

---
+++ HardArray/FourSum.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class FourSum{
+    
+    // Time Complexity: O(n^3) and Space Complexity: O(1)
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> main = new Vector<>();
+        
+        Arrays.sort(nums);
+
+        for(int i = 0; i<nums.length-3; i++){
+            if(i > 0 && nums[i-1] == nums[i]) continue;
+            for(int j = i+1; j<nums.length-2; j++){
+                if(j > i+1 && nums[j-1] == nums[j]) continue;
+                int k = j+1;
+                int l = nums.length-1;
+
+                while(k < l){
+                   long sum = (long)nums[i] + nums[j] + nums[k] + nums[l];
+
+                   if(sum < target){
+                    k++;
+                   }
+                   else if(sum > target){
+                    l--;
+                   }
+                   else{
+                    List<Integer> ans = new Vector<>(4);
+
+                    ans.add(nums[i]);
+                    ans.add(nums[j]);
+                    ans.add(nums[k]);
+                    ans.add(nums[l]);
+                    main.add(ans);
+
+                    k++;
+                    l--;
+
+                    while(nums[k-1] == nums[k] && k<l) k++;
+                    while(nums[l] == nums[l+1] && k<l) l--;
+                   }
+                }
+            }
+        }
+
+        return main;
+    }
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter size for array: ");
+        int size = input.nextInt();
+
+        int [] nums = new int[size];
+
+        System.out.println("Enter elements for array: ");
+        for(int i = 0; i<size; i++){
+            nums[i] = input.nextInt();
+        }
+         
+        System.out.println("Enter a target whom want to check 4 sum: ");
+        int target = input.nextInt();
+
+        System.out.println("Array contains following quadruplets whose sum = target: " + fourSum(nums, target));
+
+        input.close();
+    }
+}
